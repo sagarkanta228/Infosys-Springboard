ff460507282d6b35f37c7a4045e3ece7344cf7cd
sagarkanta228 <gsagarkanta@gmail.com>
2026-02-18 23:46:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from datetime import datetime
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+# Model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0
+)
+
+# Web Search Tool
+tavily_search_tool = TavilySearch(
+    max_results=3,
+    topic="general"
+)
+@tool
+def web_search(query: str) -> str:
+    """web_search tool"""
+    result = tavily_search_tool.invoke({"query": query})
+    return str(result)
+
+# Current Time Tool
+@tool
+def get_current_time() -> str:
+    """Returns the current system date and time."""
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Weather Tool
+@tool
+def get_weather(city: str, date: str) -> str:
+    """Fetch weather info using web search (semi-real)."""
+    query = f"weather in {city} on {date}"
+    return web_search(query)
+
+# Create agent
+agent = create_agent(
+    model=model,
+    tools=[web_search, get_current_time, get_weather]
+)
+
+# User query
+query = "What is the current time and latest AI news?"
+
+#Run agent
+response = agent.invoke({"messages": query})
+print(response)
